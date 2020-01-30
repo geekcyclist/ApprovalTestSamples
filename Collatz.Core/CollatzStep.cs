0546b24ee6dc53ab31e6852bd28a91c6def53cd1
Gary Ray <gary.ray@gmail.com>
2020-01-29 21:22:46 -0700

    Add Step guard clause and exception tests

--- Collatz.Core/CollatzStep.cs
+++ Collatz.Core/CollatzStep.cs
@@ -1,8 +1,24 @@
-﻿namespace Collatz.Core
+﻿using System;
+
+namespace Collatz.Core
 {
     public struct CollatzStep
     {
-        public int Value { get; set; }
+        private int _value;
+
+        public int Value
+        {
+            get { return _value; }
+            set 
+            {
+                if (value < 1)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(Value));
+                }
+                _value = value; 
+            }
+        }
+
         public int Step { get; set; }
     }
 }
