0b8bd79d9ff7a3c080aefebe26f24f9af795a5c8
Gary Ray <gary.ray@gmail.com>
2020-01-30 07:32:23 -0700

    Add series builder

---
+++ Collatz.Core.Tests/SeriesTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Xunit;
+
+namespace Collatz.Core.Tests
+{
+    public class SeriesTests
+    {
+        [Theory]
+        [InlineData(16, 4)]
+        [InlineData(35, 13)]
+        [InlineData(41, 109)]
+        public void SeriesContainsCorrectTotalSteps(int value, int expectedTotalSteps)
+        {
+            var series = new CollatzSeries(value);
+            int actualTotalSteps = series.Steps.LastOrDefault().Step;
+            Assert.Equal(expectedTotalSteps, actualTotalSteps);
+        }
+    }
+}
