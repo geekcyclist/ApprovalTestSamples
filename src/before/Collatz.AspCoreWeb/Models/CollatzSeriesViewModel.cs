1776cbd9b3e584154c225f91e6099a72c3bdbf48
Gary Ray <gary.ray@gmail.com>
2020-02-06 19:26:29 -0700

    Add initial ASP.NET Core Mvc Project
    Simple index with post to display calculator for testing.

---
+++ src/before/Collatz.AspCoreWeb/Models/CollatzSeriesViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using Collatz.Core;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Collatz.AspCoreWeb.Models
+{
+    [BindProperties]
+    public class CollatzSeriesViewModel
+    {
+        public int StartValue { get; set; }
+        public CollatzSeries Series { get; private set; }
+        public int MaxValue { get; private set; }
+        public int TotalSteps { get; private set; }
+        
+        public void Populate()
+        {
+            Series = new CollatzSeries(StartValue);
+            MaxValue = Series.Steps.Max(s => s.Value);
+            TotalSteps = Series.Steps.Last().Step;
+        }
+    }
+}
