06c5c3331b6db7062028fbe065594bceab3efc92
Gary Ray <gary.ray@gmail.com>
2020-02-11 12:42:00 -0700

    Add ApprovalTest Stubs for Collatz.Core

--- CollatzSample/src/before/Collatz.Core.Tests/SeriesTests.cs
+++ CollatzSample/src/before/Collatz.Core.Tests/SeriesTests.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 using System.Linq;
 using Xunit;
+using ApprovalTests;
+using ApprovalTests.Reporters;
 
 namespace Collatz.Core.Tests
 {
@@ -18,5 +18,49 @@ namespace Collatz.Core.Tests
             int actualTotalSteps = series.Steps.LastOrDefault().Step;
             Assert.Equal(expectedTotalSteps, actualTotalSteps);
         }
+
+        [Fact]
+        public void ValidateSeries()
+        {
+            int startValue = 10;
+            var expectedSteps = new List<CollatzStep>
+            {
+                new CollatzStep {Step = 0, Value = 10},
+                new CollatzStep {Step = 1, Value = 5},
+                new CollatzStep {Step = 2, Value = 16},
+                new CollatzStep {Step = 3, Value = 8},
+                new CollatzStep {Step = 4, Value = 4},
+                new CollatzStep {Step = 5, Value = 2},
+                new CollatzStep {Step = 6, Value = 1}
+            };
+            var actualSteps = new CollatzSeries(startValue).Steps;
+
+            for (int i = 0; i < expectedSteps.Count; i++)
+            {
+                Assert.Equal(expectedSteps[i].Step, actualSteps[i].Step);
+                Assert.Equal(expectedSteps[i].Value, actualSteps[i].Value);
+            }
+        }
+
+        [Fact (Skip = "Not Implemented")]
+        [UseReporter(typeof(DiffReporter))]
+        public void ApproveSeriesSteps()
+        {
+            //TODO: Implement ApproveSeriesSteps() to replace ValidateSeries()
+            // set the start value
+            // get an enumerable of steps
+            // verify all the steps
+        }
+
+        [Fact (Skip = "Not Implemented")]
+        [UseReporter(typeof(DiffReporter))]
+        public void ApproveSeries()
+        {
+            //TODO: Implement ApproveSeries() to approve the complex object
+            // set the start value
+            // get the series
+            // verify the series
+        }
+
     }
 }
